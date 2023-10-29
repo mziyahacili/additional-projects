e3abf40ee2ce48a4460aeae1c3e9390b987a8110
Ziya Hacili <mziyahajl@mail.ru>
2023-10-30 01:36:18 +0400

    Binding and MVVM

---
+++ WPF code/MVVMandBindingfLesson/MVVMandBindingfLesson/ViewModels/MainViewModel.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Printing;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
+using GalaSoft.MvvmLight.Command;
+using MVVMandBindingfLesson.Models;
+
+namespace MVVMandBindingfLesson.ViewModels;
+public class MainViewModel
+{
+
+
+    public Person PersonToAdd { get; set; } = new();
+    public ObservableCollection<Person> People { get; set; } = new();
+
+    public RelayCommand AddCommand 
+    { get => new(
+    () =>
+    {
+        People.Add(PersonToAdd);
+    }); }
+
+}
